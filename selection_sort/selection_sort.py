c74da3d633db6c59d4abeac2ddbd64b2b2dad0f8
Munyao Victor <munyaovictor61@gmail.com>
2025-06-20 13:52:21 +0300

    Adding a Select Sorting algorithm

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,17 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
-    pass
+    for i in range(len(arr)):
+        min_element = i
+        for j in range(i + 1, len(arr)):
+            # selecting the minimum element in each iteration
+            if arr[j] < arr[min_element]:
+                min_element = j
+        # swapping the elements to sort the array         
+        arr[i], arr[min_element] = arr[min_element], arr[i]
+    return arr
+
+# Array to be sorted
+arr = [9,3,5,78,72,2,1]
+print("Unsorted list : ",arr)
+
+print ("Sorted list", selection_sort(arr))
